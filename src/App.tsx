eb94c3fe280ec63bf38b0eaac0d0b466107663a0
pati-rosa <patirosa995@gmail.com>
2024-11-12 09:31:18 -0300

    fix: import react to deploy to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import WorkoutChart from './components/WorkoutProgressChart';
 import DailyGoalsChecklist from './components/DailyGoalsChecklist';
